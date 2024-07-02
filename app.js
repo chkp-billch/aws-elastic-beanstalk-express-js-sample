410a8e781263994d63e1138cac669014604f8ad3
Bill Cheng <billch@checkpoint.com>
2024-07-02 15:39:35 +0800

    v2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World-Bill'));
+app.get('/', (req, res) => res.send('Hello World-Bill for cloudguard code scan'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
